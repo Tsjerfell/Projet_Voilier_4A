cd1e37537f172bad28beb3342bd6f026de79fc23
Tsjerfell <67692265+Tsjerfell@users.noreply.github.com>
2021-11-08 16:39:02 +0100

    Update MySpi.c

--- MySpi.c
+++ MySpi.c
@@ -5,6 +5,7 @@ void My_Spi_init( SPI_TypeDef* Spi){
 	RCC->APB2ENR |= RCC_APB2ENR_SPI1EN; //Enable SPI1 clock
 	SPI1 ->CR1 |= (0x3>3); //Horloge de 4,5MHz. Pas de macro
 	SPI1 -> CR1 |= SPI_CR1_CPHA;
+	SPI1 -> CR1 &= ~(SPI_CR1_CPOL);
 	
 	
 }
