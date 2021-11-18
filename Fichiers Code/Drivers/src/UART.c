fac52c4a0275bacf771aba1fc743a7a4b55c2385
Sarra Kouki <kouki@insat.insa-toulouse.fr>
2021-11-18 19:40:11 +0100

    Test plateau

--- Fichiers Code/Drivers/src/UART.c
+++ Fichiers Code/Drivers/src/UART.c
@@ -1,11 +1,16 @@
 #include "UART.h"
-#include "GPIO.h"
+#include "Driver_GPIO.h"
 #include "stm32f10x.h"
 void (* Ptrf1) (void); 
 void (* Ptrf2) (void); 
 
-void UART_Init ( USART_TypeDef * UARTptr, int UART_BaudRate ) {
-		UARTptr -> CR1 |= USART_CR1_UE; //Enable l'USART 
+void UART_Init ( USART_TypeDef * UARTptr,Mode_TypeDef Mode, int UART_BaudRate ) {
+
+	if (UARTptr==USART1) RCC->APB2ENR|= RCC_APB2ENR_USART1EN; 
+	else if (UARTptr==USART2) RCC->APB1ENR|=RCC_APB1ENR_USART2EN;
+	else RCC->APB1ENR|=RCC_APB1ENR_USART3EN;
+	
+	
 		UARTptr -> CR1 &=~ USART_CR1_M; 
 		//UARTptr -> CR1 &=~ USART_CR1_PCE; 
 		UARTptr -> CR2 &=~ USART_CR2_STOP; // stop bit 
@@ -15,44 +20,56 @@ void UART_Init ( USART_TypeDef * UARTptr, int UART_BaudRate ) {
 	else if (UARTptr  == USART3 ) {
 		UARTptr  -> BRR |= (int) 36000000/(UART_BaudRate); //fréquence d'horloge de l'USAR2 = 32Mhz
 	} 
-		UARTptr  -> CR1 |= USART_CR1_TE; //Enable transmitter 
-		UARTptr  -> CR1 |= USART_CR1_RE; //Enable receiver 
+	if (Mode== TX) { 
+		UARTptr  -> CR1 |= USART_CR1_TE;
+	}
+		if (Mode== RX){//Enable transmitter 
+		UARTptr  -> CR1 |= USART_CR1_RE; 
+		}//Enable receiver 
+		UARTptr -> CR1 |= USART_CR1_UE; //Enable l'USART 
 }
 
 void UART_ActiveIT (USART_TypeDef * UARTptr, char Prio, void (* IT_Function )(void)){
 	
 	char num_USART; 
+	Ptrf1 = IT_Function;
 	
 	
 	if (UARTptr  == USART1) {
 		num_USART = 37; 
 		UARTptr -> CR1 |=  USART_CR1_RXNEIE; // Valide l'envoi d'une demande d'interruption
-		NVIC->IP[num_USART] |= (Prio << 2); // Fixe la priorité de l'interruption dans le NVIC
-		NVIC->ISER[0] |= (1 << num_USART); } // Autorise la prise en compte de l'interruption dans le NVIC
+		NVIC->IP[num_USART] = (Prio << 4); // Fixe la priorité de l'interruption dans le NVIC
+	  NVIC->ISER[1] |= NVIC_ISER_SETENA_5; } // Autorise la prise en compte de l'interruption dans le NVIC
 	
 	else if (UARTptr  == USART3) { 
 		num_USART = 39; 
 		UARTptr -> CR1 |=  USART_CR1_RXNEIE; // Valide l'envoi d'une demande d'interruption
-		NVIC->IP[num_USART] |= (Prio << 2); // Fixe la priorité de l'interruption dans le NVIC
+		NVIC->IP[num_USART] |= (Prio << 4); // Fixe la priorité de l'interruption dans le NVIC
 		NVIC->ISER[0] |= (1 << num_USART);// Autorise la prise en compte de l'interruption dans le NVIC 
 	
 	}
 }
 
-void USART1_IRQHandler ( void ){ 
+void USART1_IRQHandler ( void ){
+	USART1->SR &= ~USART_SR_RXNE; 
 	if (Ptrf1 != 0)(*Ptrf1)();
 	}
 void USART2_IRQHandler ( void ){ 
 	if (Ptrf2 != 0)(*Ptrf2)();
 	}	
-void UART_send (USART_TypeDef * UARTptr, char * data){ 
-		
-	UARTptr-> DR |= *data; // Ecriture de la donnée dans le registre DR
-	while(!(UARTptr->SR & USART_SR_TC)) {} // Attente de la fin de transmission
+void UART_send_string(USART_TypeDef * UARTptr, char * data){ 
+ while (* data != '\0'){ 
+		while(!(UARTptr->SR & USART_SR_TXE)) {} ; // Attente de la fin de transmission
+		UARTptr-> DR = *data;// Ecriture de la donnée dans le registre DR	 	
+		data++; 
+}
+ }
 
+char UART_receive (	USART_TypeDef * UARTptr) { 
+	return (char) UARTptr->DR; 
 }
 
-	
+
 
 
 	
